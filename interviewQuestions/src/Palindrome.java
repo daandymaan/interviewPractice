c6f8a1d296e39d18b67caa4477e08310178dd1af
daandymaan <sanieldimons@gmail.com>
2019-10-02 20:02:36 +0100

    Initial creation of repo and the start of the first question

---
+++ interviewQuestions/src/Palindrome.java
@@ -0,0 +1,33 @@
+//Determine if a string is a palindrome
+
+public class Palindrome 
+{
+    private String word1 = "racecar";
+    private String word2 = "Navan";
+    private String word3 = "danSimons";
+    private String randomWord;
+
+    public Palindrome()
+    {
+
+    }
+    
+
+    public boolean isPalindrome(String word)
+    {
+        if(word == null)
+        {
+            return false;
+        }
+        
+        for(i = word.length() ; i = 0 ; i-- )
+        {
+            
+        }
+        return true 
+
+
+    }
+
+    
+}
