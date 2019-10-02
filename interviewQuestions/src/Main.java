6ef640c667d0d21d8b480231d65f3771e51f5ca9
daandymaan <sanieldimons@gmail.com>
2019-10-02 20:51:29 +0100

    Finished writing comments , Big O of (n)

--- interviewQuestions/src/Main.java
+++ interviewQuestions/src/Main.java
@@ -4,7 +4,7 @@ public class Main
     public static void main(String args[])
     {
         Palindrome p = new Palindrome();
-        System.out.println(p.isPalindrome("RaceCar"));
+        System.out.println(p.isPalindrome("racecar"));
     }
 
 
--- interviewQuestions/src/Palindrome.java
+++ interviewQuestions/src/Palindrome.java
@@ -2,36 +2,35 @@
 
 public class Palindrome 
 {
-    private String word1 = "racecar";
-    private String word2 = "Navan";
-    private String word3 = "danSimons";
-    private String randomWord;
-
+    //Palindrome empty contructor method
     public Palindrome()
     {
 
     }
     
-
+    //Method to check if word entered is a palindrome 
     public boolean isPalindrome(String word)
     {
-        
+        //Initial check to see if word is null
         if(word == null)
         {
             return false;
         }
+
+        //Edge case to make sure all letters are in the same case
         String wordConverted = word.toLowerCase();
         
+        //For loop to check all letters are the same from start to finish 
         for(int i = wordConverted.length()-1,y = 0; i > 0 ; i--,y++)
         {
+            //Checks to see if each letter is equal from front to back 
            if( wordConverted.charAt(i) != wordConverted.charAt(y))
            {
                return false;
            }
         }
+        //Returns true if it passes each test 
         return true; 
-
-
     }
 
     
