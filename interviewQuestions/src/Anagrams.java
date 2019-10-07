838742a8f2925df3c5c7d9ec9ccb630d854b453d
daandymaan <sanieldimons@gmail.com>
2019-10-08 00:15:00 +0100

    interview question to check if two words are anagrams of each other, 5 mins to complete but no edge cases explored yet. Presumed that the strings cannot be null and must be the same length, will explore the other edge cases in another method Big o of n.

---
+++ interviewQuestions/src/Anagrams.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+//Check if two strings are anagrams of each other 
+
+
+public class Anagrams
+{
+    private String word1;
+    private String word2;
+    public Anagrams(String word1, String word2)
+    {
+        this.word1 = word1;
+        this.word2 = word2;
+
+    }
+
+    public boolean anagramChecker()
+    {
+
+        char temp[];
+        char temp2[];
+        int i = 0;
+
+        temp = word1.toCharArray();
+        temp2 = word2.toCharArray();
+
+        Arrays.sort(temp);
+        Arrays.sort(temp2);
+
+        System.out.println(temp);
+        System.out.println(temp2);
+
+        while(i != word1.length() || i != word2.length() )
+        {
+            if(temp[i] != temp2[i])
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+
+
+        return true;
+    }
+
+
+}
--- interviewQuestions/src/Main.java
+++ interviewQuestions/src/Main.java
@@ -3,8 +3,13 @@ public class Main
 
     public static void main(String args[])
     {
-        Palindrome p = new Palindrome();
-        System.out.println(p.isPalindrome("racecar"));
+        //Palindrome p = new Palindrome();
+        //System.out.println(p.isPalindrome("racecar"));
+
+        Anagrams a = new Anagrams("dog", "god");
+        System.out.println(a.anagramChecker());
+
+        
     }
 
 
